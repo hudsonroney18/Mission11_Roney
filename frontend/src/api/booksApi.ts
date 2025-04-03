a76333d21a8ec059004df0a4f45fb37815d1a308
hudsonroney18 <hudsonbroney@gmail.com>
2025-04-02 19:18:45 -0600

    Ready to commit

--- frontend/src/api/booksApi.ts
+++ frontend/src/api/booksApi.ts
@@ -5,7 +5,8 @@ interface FetchBooksResponse {
   totalNumBooks: number;
 }
 
-const API_URL = 'https://localhost:5000/api/Book';
+const API_URL =
+  'https://mission13-backend-hvbwgmb4ehh8akc7.westus3-01.azurewebsites.net/api/Book';
 
 export const fetchBooks = async (
   pageSize: number,
